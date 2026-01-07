50a578409b83ccb93bff4399e1c7518e128394f8
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2026-01-07 21:33:12 +0530

    chunking completed

--- rag/index.py
+++ rag/index.py
@@ -1,8 +1,18 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
 pdf_path = Path(__file__).parent / "test.pdf"
 
 loader = PyPDFLoader(file_path=pdf_path)
 docs = loader.load()
 
-print(docs[1])
+## split docs into smaller chunks
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 400
+)
+
+chunks = text_splitter.split_documents(documents=docs)

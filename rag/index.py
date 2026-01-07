7452af03a5d33f5dfb1bdb1387b3ded658b332df
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2026-01-07 21:26:07 +0530

    langchain pypdfloader setup

---
+++ rag/index.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+pdf_path = Path(__file__).parent / "test.pdf"
+
+loader = PyPDFLoader(file_path=pdf_path)
+docs = loader.load()
+
+print(docs[1])
